cf64022cddf764a02098918fc9b753843b657562
Artur Dias <arturdrm@hotmail.com>
2023-09-16 10:09:15 -0300

    ADDED: Dependency Injection extensions

---
+++ EmailAspNetCore/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using EmailAspNetCore.Contracts;
+using EmailAspNetCore.Options;
+using EmailAspNetCore.Services;
+
+namespace EmailAspNetCore;
+
+internal static class DependencyInjection
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        return services
+            .AddOptions()
+            .AddTransient<IEmailService, EmailService>();
+    }
+
+    private static IServiceCollection AddOptions(this IServiceCollection services)
+    {
+        return services
+            .ConfigureOptions<EmailOptionsSetup>();
+    }
+}
