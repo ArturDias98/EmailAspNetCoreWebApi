eede5e248dcb13f80118ecb81a946bf632cfa3ae
Artur Dias <arturdrm@hotmail.com>
2023-09-16 10:05:46 -0300

    ADDED: Inject email options in email service

--- EmailAspNetCore/Services/EmailService.cs
+++ EmailAspNetCore/Services/EmailService.cs
@@ -1,9 +1,18 @@
-﻿using EmailAspNetCore.Contracts;
+﻿using EmailAspNetCore.Configuration;
+using EmailAspNetCore.Contracts;
+using Microsoft.Extensions.Options;
 
 namespace EmailAspNetCore.Services;
 
 internal class EmailService : IEmailService
 {
+    private readonly EmailOptions _options;
+
+    public EmailService(IOptions<EmailOptions> options)
+    {
+        _options = options.Value;
+    }
+
     public Task SendAsync(string subject, string body, IEnumerable<string> to, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
