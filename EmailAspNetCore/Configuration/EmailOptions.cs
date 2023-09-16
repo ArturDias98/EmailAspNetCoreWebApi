4f92171bc3ec05198667c73ec469c25ec312caf6
Artur Dias <arturdrm@hotmail.com>
2023-09-16 10:04:17 -0300

    ADDED: Email options

---
+++ EmailAspNetCore/Configuration/EmailOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailAspNetCore.Configuration;
+
+internal class EmailOptions
+{
+    public const string Section = "Email";
+    public string Host { get; set; } = string.Empty;
+    public int Port { get; set; }
+    public string User { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
---
+++ EmailAspNetCore/Options/EmailOptionsSetup.cs
@@ -0,0 +1,21 @@
+﻿using EmailAspNetCore.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace EmailAspNetCore.Options;
+
+internal class EmailOptionsSetup : IConfigureOptions<EmailOptions>
+{
+    private readonly IConfiguration _configuration;
+
+    public EmailOptionsSetup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Configure(EmailOptions options)
+    {
+        _configuration
+            .GetSection(EmailOptions.Section)
+            .Bind(options);
+    }
+}
