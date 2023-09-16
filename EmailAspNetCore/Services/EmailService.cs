8378df3c180a707273c50306986f7421c802dbd9
Artur Dias <arturdrm@hotmail.com>
2023-09-16 10:43:57 -0300

    ADDED: Email service implementation

--- EmailAspNetCore/Services/EmailService.cs
+++ EmailAspNetCore/Services/EmailService.cs
@@ -1,6 +1,10 @@
 ﻿using EmailAspNetCore.Configuration;
 using EmailAspNetCore.Contracts;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using Microsoft.Extensions.Options;
+using MimeKit;
+using MimeKit.Text;
 
 namespace EmailAspNetCore.Services;
 
@@ -13,8 +17,30 @@ internal class EmailService : IEmailService
         _options = options.Value;
     }
 
-    public Task SendAsync(string subject, string body, IEnumerable<string> to, CancellationToken cancellationToken = default)
+    public async Task SendAsync(
+        string subject,
+        string body,
+        IEnumerable<string> to,
+        CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        using var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse(_options.User));
+        var parseTo = to.Select(i => MailboxAddress.Parse(i));
+        email.To.AddRange(parseTo);
+
+        email.Subject = subject;
+        email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+        using var smtp = new SmtpClient();
+        await smtp.ConnectAsync(
+            _options.Host,
+            _options.Port,
+            SecureSocketOptions.StartTls,
+            cancellationToken);
+        await smtp.AuthenticateAsync(
+             _options.User,
+             _options.Password,
+             cancellationToken);
+        await smtp.SendAsync(email);
     }
 }
